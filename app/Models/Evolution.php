1b8d68a2ed26be1b72cd8d9ff9538191c9ba1a1c
WaveHack <email@wavehack.net>
2021-08-16 23:44:00 +0200

    Update Location and Evolution models with base model and relations

--- app/Models/Evolution.php
+++ app/Models/Evolution.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Evolution extends Model
 {
@@ -12,4 +12,9 @@ class Evolution extends Model
     protected $casts = [
         'requirements' => 'array',
     ];
+
+    public function locations(): HasMany
+    {
+        return $this->hasMany(Location::class);
+    }
 }
