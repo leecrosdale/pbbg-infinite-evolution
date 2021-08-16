7c0aaa6e8d39a2c848b1b0f67f3ffc0c9cc654c3
WaveHack <email@wavehack.net>
2021-08-16 23:39:10 +0200

    Refactor DatabaseSeeder.php to only call seeders instead of factories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Evolution;
-use App\Models\Location;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,20 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-
-        if (config('app.env') === 'local')
-        {
-            Evolution::factory(10)->create();
-            Location::factory(10)->create();
-        }
-        else
-        {
-            $this->call(
-                EvolutionTableSeeder::class,
-                LocationTableSeeder::class
-            );
-        }
+        $this->call([
+            EvolutionTableSeeder::class,
+            LocationTableSeeder::class,
+        ]);
     }
 }
