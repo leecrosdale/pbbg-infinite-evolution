5f374f984616b0d783f6e6440fe7295a5e9ef1c1
WaveHack <email@wavehack.net>
2021-08-18 19:10:45 +0200

    Condense locations page a bit

--- resources/views/pages/location.blade.php
+++ resources/views/pages/location.blade.php
@@ -2,50 +2,55 @@
 
 @section('content')
     <div class="container">
+        <div class="row">
 
-        @foreach ($evolutions as $evolution)
-            <h2>{{ $evolution->name }}</h2>
+            @foreach ($evolutions as $evolution)
+                <div class="col-12 col-md-6">
 
-            @foreach ($evolution->locations as $location)
-                <x-card header="Location: {{ $location->name }}" class="mb-3">
+                    <h2>{{ $evolution->name }}</h2>
 
-                    @if ($location->characters->count() === 0)
-                        No characters detected.
-                    @else
-                        <x-slot name="bodyClass">p-0 table-responsive</x-slot>
+                    @foreach ($evolution->locations as $location)
+                        <x-card header="Location: {{ $location->name }}" class="mb-3">
 
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Character</th>
-                                    <th class="text-right">Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($location->characters as $locationCharacter)
-                                    <tr>
-                                        <td class="align-middle">
-                                            {{ $locationCharacter->name }}
-                                            @if ($locationCharacter->id === $character->id)
-                                                <span class="badge badge-primary">You</span>
-                                            @endif
-                                        </td>
-                                        <td class="text-right align-middle">
-                                            @if ($locationCharacter->id !== $character->id)
-                                                <a href="#" class="btn btn-sm btn-primary">Message</a>
-                                                <a href="#" class="btn btn-sm btn-danger">Attack</a>
-                                            @endif
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                    @endif
+                            @if ($location->characters->count() === 0)
+                                No characters detected.
+                            @else
+                                <x-slot name="bodyClass">p-0 table-responsive</x-slot>
 
-                </x-card>
+                                <table class="table">
+                                    <thead>
+                                        <tr>
+                                            <th>Character</th>
+                                            <th class="text-right">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($location->characters as $locationCharacter)
+                                            <tr>
+                                                <td class="align-middle">
+                                                    {{ $locationCharacter->name }}
+                                                    @if ($locationCharacter->id === $character->id)
+                                                        <span class="badge badge-primary">You</span>
+                                                    @endif
+                                                </td>
+                                                <td class="text-right align-middle">
+                                                    @if ($locationCharacter->id !== $character->id)
+                                                        <a href="#" class="btn btn-sm btn-primary">Message</a>
+                                                        <a href="#" class="btn btn-sm btn-danger">Attack</a>
+                                                    @endif
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            @endif
+
+                        </x-card>
+                    @endforeach
+
+                </div>
             @endforeach
 
-        @endforeach
-
+        </div>
     </div>
 @endsection
