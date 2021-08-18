bb300860ec0eb32b6d97ed5a275a10c626b280ec
WaveHack <email@wavehack.net>
2021-08-18 18:35:38 +0200

    Create CharacterFactory for creating characters, fixes #3

--- app/Listeners/CreateCharacter.php
+++ app/Listeners/CreateCharacter.php
@@ -2,32 +2,23 @@
 
 namespace App\Listeners;
 
-use App\Models\Evolution;
+use App\Factories\CharacterFactory;
+use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 
 class CreateCharacter
 {
-    public function handle(Registered $event)
+    public function __construct(
+        private CharacterFactory $factory
+    )
     {
-        $evolution = Evolution::first();
-        $location = $evolution->locations()->inRandomOrder()->first();
+    }
 
-        $event->user->characters()->create([
-            'evolution_id' => $evolution->id,
-            'location_id' => $location->id,
+    public function handle(Registered $event): void
+    {
+        /** @var User $user */
+        $user = $event->user;
 
-            'name' => $event->user->name,
-
-            'level' => 0,
-            'experience' => 0,
-
-            'health' => 500,
-            'max_health' => 500,
-            'energy' => 100,
-            'max_energy' => 100,
-
-            'strength' => 1,
-            'stamina' => 1,
-        ]);
+        $this->factory->createForUser($user);
     }
 }
