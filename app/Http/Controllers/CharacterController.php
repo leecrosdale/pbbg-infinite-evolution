05d6f87a3102ab2384c1bb4f6d588d72363b72b8
WaveHack <email@wavehack.net>
2021-08-19 21:58:26 +0200

    Add docblock

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Character;
+
 class CharacterController extends Controller
 {
     public function travelling()
     {
+        /** @var Character $character */
         $character = auth()->user()->character;
 
         return view('pages.travelling', compact('character'));
