28067f4e04a710dc0ff3297c2278d848878da7d8
WaveHack <email@wavehack.net>
2021-08-19 21:16:32 +0200

    Cleanup CheckCharacterStatus.php

--- app/Http/Middleware/CheckCharacterStatus.php
+++ app/Http/Middleware/CheckCharacterStatus.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use App\Enums\CharacterStatus;
+use App\Models\Character;
 use Closure;
 use Illuminate\Http\Request;
 
@@ -11,27 +12,23 @@ class CheckCharacterStatus
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param Request $request
+     * @param Closure $next
      * @return mixed
      */
     public function handle(Request $request, Closure $next)
     {
+        /** @var Character $character */
+        $character = $request->user()->character;
 
-        if ($request->user()->character->status !== CharacterStatus::FREE)
-        {
-            switch ($request->user()->character->status)
-            {
+        if ($character->status !== CharacterStatus::FREE) {
+            switch ($character->status) {
                 case CharacterStatus::TRAVELLING:
-
-                    if (!$request->user()->character->canBeFreed()) {
+                    if (!$character->canBeFreed()) {
                         return redirect()->route('character.travelling');
                     }
-
-                break;
-
+                    break;
             }
-
         }
 
         return $next($request);
