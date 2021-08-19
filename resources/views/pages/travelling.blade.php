7f2c9526036e4c5e91cc49b494d42e807ec56d50
WaveHack <email@wavehack.net>
2021-08-19 21:42:40 +0200

    Cleanup traveling view

--- resources/views/pages/travelling.blade.php
+++ resources/views/pages/travelling.blade.php
@@ -6,20 +6,12 @@
 
             <div class="col-12 col-md-12">
                 <x-card header="Travelling" class="mb-3">
-                    <x-slot name="bodyClass">p-2 mb-3 table-responsive</x-slot>
-
                     @if (!$character->canBeFreed())
-
-                    You are currently travelling and will arrive in {{ $character->free_time }}
-
+                        You are currently travelling to {{ $location->name }} and will arrive in {{ $character->free_time }}.
                     @else
-
-                    You have arrived
-
+                        You have arrived
                     @endif
                 </x-card>
-
-
             </div>
 
         </div>
