ede522b6a6f7f9f62cd3b2098c9beb314903ffdf
WaveHack <email@wavehack.net>
2021-08-19 21:12:33 +0200

    Move permadeath todo/idea to Trello: https://trello.com/c/Y64qNZUx/58-character-permadeath

--- app/Enums/CharacterStatus.php
+++ app/Enums/CharacterStatus.php
@@ -7,6 +7,5 @@ class CharacterStatus
     public const FREE = 0;
     public const TRAVELLING = 1;
     public const BATTLING = 2;
-    public const DEAD = 3; // TODO - permadeath??
-
+    public const DEAD = 3;
 }
