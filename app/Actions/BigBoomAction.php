0bf5ef4eeed7d6cc24cd66255ba79aeaf6413e53
WaveHack <email@wavehack.net>
2021-08-20 01:16:14 +0200

    Add BigBoomAction.php

---
+++ app/Actions/BigBoomAction.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Actions;
+
+use App\Models\Character;
+
+class BigBoomAction
+{
+    public function __invoke(Character $character): void
+    {
+        // todo
+
+//        $round = $character->round;
+//
+//        $this->endRound($round);
+//        $this->updateLeaderboard($round);
+//        $this->sendCharactersBack($round);
+//        $this->lowerCharacterStats($round);
+//        $this->removeCharacterItems($round);
+//        $this->destroyBombedLocationsAndGenerateNewOnes($round);
+//        $this->createNextRound($round);
+    }
+}
