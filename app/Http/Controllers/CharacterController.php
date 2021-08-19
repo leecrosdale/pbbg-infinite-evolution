0026fc8d805c8ff3f70520c27ec7206bc595b727
WaveHack <email@wavehack.net>
2021-08-19 21:16:24 +0200

    Cleanup CharacterController.php

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class CharacterController extends Controller
 {
     public function travelling()
     {
-        return view('pages.travelling')->with('character', auth()->user()->character);
+        $character = auth()->user()->character;
+
+        return view('pages.travelling', compact('character'));
     }
 }
