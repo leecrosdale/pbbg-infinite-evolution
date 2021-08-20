2e517ebc3f782730e54aba194c328d1250ed3f72
WaveHack <email@wavehack.net>
2021-08-21 00:04:17 +0200

    Working on building actions

--- app/Actions/ConstructBuildingAction.php
+++ app/Actions/ConstructBuildingAction.php
@@ -2,10 +2,25 @@
 
 namespace App\Actions;
 
+use App\Enums\BuildingType;
+use App\Exceptions\GameException;
+use App\Models\Character;
+
 class ConstructBuildingAction
 {
-    public function __invoke()
+    public function __invoke(Character $character, string $buildingType)
     {
+        $this->guardAgainstInvalidBuildingType($buildingType);
+        // ensure char doesnt have building already at their location
+        // ensure construction costs are affordable
+
         // TODO: Implement __invoke() method.
+    }
+
+    private function guardAgainstInvalidBuildingType(string $buildingType): void
+    {
+        if (!in_array($buildingType, BuildingType::$buildingTypes, true)) {
+            throw new GameException('You cannot construct that building type.');
+        }
     }
 }
--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\ConstructBuildingAction;
 use App\Actions\UpgradeBuildingAction;
+use App\Calculator\ConstructBuildingCalculator;
+use App\Calculator\UpgradeBuildingCalculator;
+use App\Exceptions\GameException;
 use App\Http\Requests\ConstructBuildingActionRequest;
 use App\Http\Requests\UpgradeBuildingActionRequest;
 use App\Models\Character;
@@ -18,16 +22,44 @@ class BuildingController extends Controller
             ->where('location_id', $character->location_id)
             ->get();
 
-        return view('pages.buildings', compact('buildings'));
+        return view('pages.buildings', compact('buildings'))
+            ->with([
+                'constructBuildingCalculator' => app(ConstructBuildingCalculator::class),
+                'upgradeBuildingCalculator' => app(UpgradeBuildingCalculator::class),
+            ]);
     }
 
-    public function construct(ConstructBuildingActionRequest $request, UpgradeBuildingAction $action)
+    public function construct(ConstructBuildingActionRequest $request, ConstructBuildingAction $action)
     {
-        $action();
+        /** @var Character $character */
+        $character = auth()->user()->character;
+        $buildingType = $request->get('building_type');
+
+        try {
+            $action($character, $buildingType);
+
+        } catch (GameException $e) {
+            return redirect()->back()
+                ->withErrors($e->getMessage());
+        }
+
+        return redirect()->route('buildings');
     }
 
     public function upgrade(UpgradeBuildingActionRequest $request, UpgradeBuildingAction $action)
     {
-        $action();
+        /** @var Character $character */
+        $character = auth()->user()->character;
+        $buildingType = $request->get('building_type');
+
+        try {
+            $action($character, $buildingType);
+
+        } catch (GameException $e) {
+            return redirect()->back()
+                ->withErrors($e->getMessage());
+        }
+
+        return redirect()->route('buildings');
     }
 }
