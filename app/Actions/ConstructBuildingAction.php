59491020cc80d881fcca9ae746459da027141984
WaveHack <email@wavehack.net>
2021-08-21 00:21:18 +0200

    Working on ConstructBuildingAction

--- app/Actions/ConstructBuildingAction.php
+++ app/Actions/ConstructBuildingAction.php
@@ -2,17 +2,24 @@
 
 namespace App\Actions;
 
+use App\Calculator\ConstructBuildingCalculator;
 use App\Enums\BuildingType;
 use App\Exceptions\GameException;
 use App\Models\Character;
 
 class ConstructBuildingAction
 {
+    public function __construct(
+        private ConstructBuildingCalculator $calculator,
+    )
+    {
+    }
+
     public function __invoke(Character $character, string $buildingType)
     {
         $this->guardAgainstInvalidBuildingType($buildingType);
-        // ensure char doesnt have building already at their location
-        // ensure construction costs are affordable
+        $this->guardAgainstAlreadyConstructedBuilding($character, $buildingType);
+        $this->guardAgainstUnaffordableConstructionCosts($character, $buildingType);
 
         // TODO: Implement __invoke() method.
     }
@@ -22,5 +29,24 @@ class ConstructBuildingAction
         if (!in_array($buildingType, BuildingType::$buildingTypes, true)) {
             throw new GameException('You cannot construct that building type.');
         }
+    }
+
+    private function guardAgainstAlreadyConstructedBuilding(Character $character, string $buildingType): void
+    {
+        $hasBuilding = $character->buildings()->where([
+            'location_id' => $character->location->id,
+            'type' => $buildingType,
+        ])->exists();
+
+        if ($hasBuilding) {
+            throw new GameException("You already have a {$buildingType} at {$character->location->name}.");
+        }
+    }
+
+    private function guardAgainstUnaffordableConstructionCosts(Character $character, string $buildingType): void
+    {
+        $requiredSupplies = $this->calculator->getSupplyCosts($buildingType);
+
+        // todo
     }
 }
--- app/Calculator/ConstructBuildingCalculator.php
+++ app/Calculator/ConstructBuildingCalculator.php
@@ -3,7 +3,34 @@
 namespace App\Calculator;
 
 use App\Enums\BuildingType;
+use JetBrains\PhpStorm\Pure;
 
 class ConstructBuildingCalculator
 {
+    private array $constructionCosts = [
+        BuildingType::FARM => [
+            SupplyType::FOOD => 10,
+            SupplyType::WOOD => 10,
+            SupplyType::STONE => 30,
+        ],
+        BuildingType::LUMBER_YARD => [
+            SupplyType::FOOD => 10,
+            SupplyType::WOOD => 40,
+        ],
+        BuildingType::MINE => [
+            SupplyType::FOOD => 10,
+            SupplyType::WOOD => 20,
+            SupplyType::STONE => 20,
+        ],
+    ];
+
+    /**
+     * @param string $buildingType
+     * @return array{ foo: string, bar: string }
+     */
+    #[Pure]
+    public function getSupplyCosts(string $buildingType): array
+    {
+        return [];
+    }
 }
