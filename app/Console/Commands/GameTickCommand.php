46b0328a698b7def2285149f7bb7233c9eed0f10
WaveHack <email@wavehack.net>
2021-08-22 14:58:51 +0200

    Add game:tick scheduled command for passive energy/health regen

---
+++ app/Console/Commands/GameTickCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Character;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class GameTickCommand extends Command
+{
+    private const ENERGY_PER_TICK = 2;
+    private const HEALTH_PERCENTAGE_PER_TICK = 1;
+
+    protected $signature = 'game:tick';
+
+    protected $description = 'Performs a game tick (every 5 mins)';
+
+    public function handle()
+    {
+        DB::transaction(function () {
+            $characters = Character::all();
+
+            foreach ($characters as $character) {
+                $character->energy = min(
+                    $character->energy + static::ENERGY_PER_TICK,
+                    $character->max_energy
+                );
+
+                $character->health = min(
+                    $character->health + ($character->max_health / 100) * static::HEALTH_PERCENTAGE_PER_TICK,
+                    $character->max_health
+                );
+
+                $character->save();
+            }
+        });
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,29 +2,22 @@
 
 namespace App\Console;
 
+use App\Console\Commands\GameTickCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
     /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        //
-    ];
-
-    /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(GameTickCommand::class)
+            ->everyFiveMinutes();
     }
 
     /**
@@ -34,8 +27,6 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
-
-        require base_path('routes/console.php');
+        $this->load(__DIR__ . '/Commands');
     }
 }
