a399b0d38286e7822daa74098c9e9e516b49c92e
WaveHack <email@wavehack.net>
2021-08-22 23:48:56 +0200

    Use Vue timer-component on status pages

--- resources/views/pages/status/travelling.blade.php
+++ resources/views/pages/status/travelling.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <x-card header="Travelling" class="mb-3">
         @if ($character->status === \App\Enums\CharacterStatus::TRAVELLING)
-            You are currently travelling to {{ $location->name }} and will arrive in <span id="arrival_seconds">{{ $secondsRemaining }}</span> seconds.
+            <p class="mb-0">You are currently travelling to {{ $location->name }} and will arrive in <timer-component seconds="{{ $secondsRemaining }}" reload-on-finish="true">0</timer-component> seconds.</p>
         @else
             <p>You have arrived at {{ $location->name }}.</p>
             <p class="mb-0">
@@ -12,24 +12,3 @@
         @endif
     </x-card>
 @endsection
-
-@push('scripts')
-    <script>
-        var secondsRemaining = {{ $secondsRemaining }};
-
-        if (secondsRemaining > 0) {
-            window.addEventListener('DOMContentLoaded', function() {
-                window.setInterval(function () {
-                    secondsRemaining--;
-
-                    if (secondsRemaining < 0) {
-                        location.reload();
-                        return;
-                    }
-
-                    $('#arrival_seconds').text(secondsRemaining);
-                }, 1000);
-            });
-        }
-    </script>
-@endpush
