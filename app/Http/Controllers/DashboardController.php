4dd0fd9eb69f8d3e8ffd99661074e6d66f5213f3
WaveHack <email@wavehack.net>
2021-08-26 00:23:58 +0200

    WIP dashboard page

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Calculator\UpgradeBuildingCalculator;
+use App\Calculator\WorkBuildingCalculator;
+use App\Models\Character;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.dashboard');
+        /** @var Character $character */
+        $character = auth()->user()->character;
+
+        $buildings = $character->buildings()
+            ->where('location_id', $character->location_id)
+            ->get();
+
+        return view('pages.dashboard', compact(
+            'buildings',
+        ))->with([
+            'upgradeBuildingCalculator' => app(UpgradeBuildingCalculator::class),
+            'workBuildingCalculator' => app(WorkBuildingCalculator::class),
+        ]);
     }
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,6 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
+    <h3>Buildings</h3>
+
+    @if ($buildings->count() === 0)
+        <p>You don't have any buildings constructed here at {{ $location->name }}.</p>
+        <p>Head to the <a href="{{ route('buildings') }}">buildings page</a> to construct some.</p>
+    @else
+        <div class="row">
+            @foreach ($buildings as $building)
+                <div class="col-12 col-md-6 col-xl-4 mb-4">
+                    <x-card>
+                        <x-slot name="header">
+                            {{ snake_case_to_words($building->type) }}
+                            <span class="float-right text-muted">Lv.{{ number_format($building->level) }}</span>
+                        </x-slot>
+
+                        <x-slot name="bodyClass">p-2</x-slot>
+
+                        <div class="mb-2">
+                            <div class="font-weight-bold">Health:</div>
+                            <div class="progress">
+                                <div class="progress-bar bg-danger" role="progressbar" style="width: {{ floor(($building->health / $building->max_health) * 100) }}%;"></div>
+                            </div>
+                            <span class="d-block text-right">
+                                {{ number_format($building->health) }} / {{ number_format($building->max_health) }}
+                            </span>
+                        </div>
+
+                        <progress-timer-component start-time="{{ $building->work_started_at }}" current-time="{{ now() }}" end-time="{{ $building->next_work_at }}">
+                            <div class="row">
+                                <div class="col-6">
+                                    @php ($energyCostToWork = $workBuildingCalculator->getEnergyCost($character, $building->type))
+                                    <form action="{{ route('buildings.work') }}" method="POST" class="d-inline-block">
+                                        @csrf
+
+                                        <input type="hidden" name="building_type" value="{{ $building->type }}">
+                                        <button type="submit" class="btn btn-sm btn-success" {{ $character->energy < $energyCostToWork ? 'disabled' : null }}>
+                                            Work (-{{ number_format($energyCostToWork) }}e)
+                                        </button>
+                                    </form>
+                                </div>
+                                <div class="col-6">
+                                    <form action="{{ route('buildings.upgrade') }}" method="POST" class="d-inline-block">
+                                        @csrf
+
+                                        <input type="hidden" name="building_type" value="{{ $building->type }}">
+                                        <button type="submit" class="btn btn-sm btn-warning" {{ !$upgradeBuildingCalculator->canAffordUpgrade($character, $building) ? 'disabled' : null }}>
+                                            Upgrade (-{{ $upgradeBuildingCalculator->getEnergyCost($character, $building->type) }}e)
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </progress-timer-component>
+                    </x-card>
+                </div>
+            @endforeach
+        </div>
+    @endif
+
+    <h3 class="mt-4">Other Players</h3>
+    buildings here
+
+    other players
+
+
     dashboard todo
     {{--<x-card header="Buildings at {{ $location->name }}">
         @php($buildings = $character->buildings()->where('location_id', $location->id)->get())
