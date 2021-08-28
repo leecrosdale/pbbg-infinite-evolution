aa7afb0dc438e2c68b491add5c1e607108ec9952
WaveHack <email@wavehack.net>
2021-08-28 03:20:10 +0200

    Restyle buildings page, 'construct new buildings' part

--- resources/views/pages/buildings.blade.php
+++ resources/views/pages/buildings.blade.php
@@ -1,6 +1,98 @@
 @extends('layouts.app')
 
+@section('description')
+    <p>The buildings page lists all your constructed buildings, as well as other buildings you construct in your current location.</p>
+    <p class="mb-0">Different buildings can be constructed in different locations.</p>
+@endsection
+
 @section('content')
+    <h1>Buildings</h1>
+
+    @if ($buildings->count() === 0)
+        <p class="my-4 text-xl">You don't have any buildings constructed here at {{ $location->name }}.</p>
+    @else
+        todo buildings here
+    @endif
+
+    <h2>Construct</h2>
+
+    <div class="row">
+        @foreach (\App\Enums\BuildingType::all() as $buildingType)
+            @if ($buildingType === \App\Enums\BuildingType::SCAVENGERS_HUT)
+                @continue
+            @endif
+
+            @php($building = $character->getBuilding($buildingType))
+
+            @if ($building !== null)
+                @continue
+            @endif
+
+            <div class="col-12 col-lg-6 mb-3">
+                <div class="card mt-3 mt-lg-0">
+                    <div class="card-header px-3 text-white bg-secondary">
+                        <div class="d-flex">
+                            <div class="position-relative rounded-circle bg-white" style="width: 2.5rem; height: 2.5rem;">
+                                <img src="{{ asset("img/icons/buildings/{$buildingType}.svg") }}"
+                                     alt=""
+                                     class="d-block position-absolute"
+                                     style="height: 2rem; margin: 0.25rem;">
+                            </div>
+                            <div class="ml-2 d-flex flex-grow-1 flex-column">
+                                <div class="font-weight-bold">{{ snake_case_to_words($buildingType) }}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body p-3">
+
+                        <div class="font-weight-bold border-bottom">Construct</div>
+                        <div class="row mt-2">
+                            <div class="col-6 d-flex flex-wrap align-items-start">
+                                @foreach ($constructBuildingCalculator->getSupplyCosts($buildingType) as $supplyType => $requiredAmount)
+                                    <div class="d-flex align-items-center mr-2">
+                                        <img src="{{ asset("img/icons/supplies/{$supplyType}.svg") }}"
+                                             alt="{{ snake_case_to_words($supplyType) }}"
+                                             style="height: 1rem;">
+                                        <span class="ml-1 text-danger">-{{ number_format($requiredAmount) }}</span>
+                                    </div>
+                                @endforeach
+                            </div>
+                            <div class="col-6">
+                                <form action="{{ route('buildings.construct') }}" method="POST">
+                                    @csrf
+
+                                    <input type="hidden" name="building_type" value="{{ $buildingType }}">
+                                    <button type="submit" class="btn btn-block btn-primary" {{ (!$constructBuildingCalculator->canAffordConstruction($character, $buildingType) || ($character->energy < $constructBuildingCalculator->getEnergyCost($character, $buildingType))) ? 'disabled' : null }}>
+                                        <div>Construct</div>
+                                        <small class="d-flex justify-content-center">
+                                            <div><i class="fas fa-bolt"></i> -{{ $constructBuildingCalculator->getEnergyCost($character, $buildingType) }}</div>
+                                        </small>
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+
+                        <div class="font-weight-bold border-bottom">Work</div>
+                        <div class="row mt-2">
+                            <div class="col-6 d-flex flex-wrap align-items-start">
+                                @foreach ($workBuildingCalculator->getSupplyGains($buildingType) as $supplyType => $amount)
+                                    <div class="d-flex align-items-center mr-2">
+                                        <img src="{{ asset("img/icons/supplies/{$supplyType}.svg") }}"
+                                             alt="{{ snake_case_to_words($supplyType) }}"
+                                             style="height: 1rem;">
+                                        <span class="ml-1 text-success">+{{ number_format($amount) }}</span>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+    <br><br><br>
 
     <x-card header="Buildings at {{ $location->name }}" class="mb-4">
         @if ($buildings->count() === 0)
@@ -69,59 +161,4 @@
             </table>
         @endif
     </x-card>
-
-    <div class="row mb-0 mb-lg-4">
-        @foreach (\App\Enums\BuildingType::all() as $buildingType)
-            @if ($buildingType === \App\Enums\BuildingType::SCAVENGERS_HUT)
-                @continue
-            @endif
-
-            @php($building = $character->getBuilding($buildingType))
-
-            @if ($building !== null)
-                @continue
-            @endif
-
-            <div class="col-12 col-md-4 col-lg-6 mb-4">
-                <x-card header="{{ snake_case_to_words($buildingType) }}">
-                    <p>Constructing a {{ snake_case_to_words($buildingType) }} here will cost you:</p>
-
-                    <ul>
-                        @foreach ($constructBuildingCalculator->getSupplyCosts($buildingType) as $supplyType => $requiredAmount)
-                            <li>
-                                {{ number_format($requiredAmount) }}x
-                                {{--<img src="{{ asset("images/supplies/{$supplyType}.png") }}"
-                                     alt="{{ snake_case_to_words($supplyType) }}"
-                                     style="height: 1rem; width: auto;">--}}
-                                {{ snake_case_to_words($supplyType) }}
-                            </li>
-                        @endforeach
-                    </ul>
-
-                    <p>Working at the {{ snake_case_to_words($buildingType) }} here will gain you:</p>
-
-                    <ul>
-                        @foreach ($workBuildingCalculator->getSupplyGains($buildingType) as $supplyType => $amount)
-                            <li>
-                                {{ number_format($amount) }}x
-                                {{--<img src="{{ asset("images/supplies/{$supplyType}.png") }}"
-                                     alt="{{ snake_case_to_words($supplyType) }}"
-                                     style="height: 1rem; width: auto;">--}}
-                                {{ snake_case_to_words($supplyType) }}
-                            </li>
-                        @endforeach
-                    </ul>
-
-                    <form action="{{ route('buildings.construct') }}" method="POST">
-                        @csrf
-
-                        <input type="hidden" name="building_type" value="{{ $buildingType }}">
-                        <button type="submit" class="btn btn-primary" {{ (!$constructBuildingCalculator->canAffordConstruction($character, $buildingType) || ($character->energy < $constructBuildingCalculator->getEnergyCost($character, $buildingType))) ? 'disabled' : null }}>
-                            Construct (-{{ $constructBuildingCalculator->getEnergyCost($character, $buildingType) }}e)
-                        </button>
-                    </form>
-                </x-card>
-            </div>
-        @endforeach
-    </div>
 @endsection
