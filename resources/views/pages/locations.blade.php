cafe73807ff5598bdaaeaf15edcf6016636bff10
WaveHack <email@wavehack.net>
2021-08-28 13:22:20 +0200

    Remove other players from location page

--- resources/views/pages/locations.blade.php
+++ resources/views/pages/locations.blade.php
@@ -1,59 +1,23 @@
 @extends('layouts.app')
 
+@section('description')
+    <p>The locations page shows you other locations you can travel to.</p>
+    <p class="mb-0">The work column shows your buildings in each location which can be worked at.</p>
+@endsection
+
 @section('content')
+    <h1>Locations</h1>
 
-    <x-card header="{{ $location->name }}" class="mb-4">
-        <p class="mb-0">You are currently at {{ $location->name }}.</p>
-
-        {{--@if ($otherCharacters->count() > 0)
-            <p class="mb-0">You see {{ number_format($otherCharacters->count()) }} other {{ Str::plural('player', $otherCharacters->count()) }} around you.</p>
-        @endif--}}
-    </x-card>
-
-    @if ($otherCharacters->count() > 0)
-        <x-card header="Other players at {{ $location->name }}" class="mb-4">
-            <x-slot name="bodyClass">p-0 table-responsive</x-slot>
-
-            <table class="table table-hover mb-0">
-                <thead>
-                    <tr>
-                        <th>Player</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($otherCharacters as $otherCharacter)
-                        <tr>
-                            <td>
-                                <div>{{ $otherCharacter->name }}</div>
-                                <small class="text-secondary">Level {{ $otherCharacter->level }}</small>
-                            </td>
-                            <td>
-                                <form action="{{ route('character.attack', $otherCharacter) }}" method="POST">
-                                    @csrf
-
-                                    <button type="submit" class="btn btn-sm btn-danger" {{ $character->energy < $attackCharacterCalculator->getEnergyCost($character) ? 'disabled' : null }}>
-                                        Attack (-{{ $attackCharacterCalculator->getEnergyCost($character) }}e)
-                                    </button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </x-card>
-    @endif
-
-    <x-card header="Other locations" class="mb-4 mb-lg-0">
+    <x-card class="mb-4">
         <x-slot name="bodyClass">p-0 table-responsive</x-slot>
 
         <table class="table table-hover mb-0">
-            <thead>
+            <thead class="text-white bg-secondary">
                 <tr>
                     <th>Location</th>
-                    <th>Available Work</th>
-                    <th>Players</th>
-                    <th>Action</th>
+                    <th>Work</th>
+                    <th class="text-right">Players</th>
+                    <th width="130">Actions</th>
                 </tr>
             </thead>
             <tbody>
@@ -76,7 +40,7 @@
                                     @endif
                                 @endforeach
                             </td>
-                            <td>{{ $evolutionLocation->characters->count() }}</td>
+                            <td class="text-right">{{ $evolutionLocation->characters->count() }}</td>
                             <td>
                                 @if ($character->location->id !== $evolutionLocation->id)
                                     @php($energyCost = $travelCalculator->getEnergyCost($character, $evolutionLocation))
@@ -84,9 +48,12 @@
                                     <form action="{{ route('locations.travel', $evolutionLocation) }}" method="POST">
                                         @csrf
 
-                                        <button type="submit" class="btn btn-sm btn-primary" {{ $energyCost > $character->energy ? 'disabled' : null }}>
-                                            Travel
-                                            (-{{ number_format($energyCost) }}e, {{ number_format($travelCalculator->getTravelTimeInSeconds($character, $evolutionLocation)) }}s)
+                                        <button type="submit" class="btn btn-block btn-primary" {{ $energyCost > $character->energy ? 'disabled' : null }}>
+                                            <div>Travel</div>
+                                            <small class="d-flex justify-content-center">
+                                                <div><i class="fas fa-bolt"></i> -{{ number_format($energyCost) }}</div>
+                                                <span class="ml-3"><i class="fas fa-hourglass"></i> {{ number_format($travelCalculator->getTravelTimeInSeconds($character, $evolutionLocation)) }}</span>
+                                            </small>
                                         </button>
                                     </form>
                                 @endif
@@ -97,5 +64,4 @@
             </tbody>
         </table>
     </x-card>
-
 @endsection
