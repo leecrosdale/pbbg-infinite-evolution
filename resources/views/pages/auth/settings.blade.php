3c689a20cd09ea8f4a9bf7e9eff00a78228cb5f3
WaveHack <email@wavehack.net>
2021-08-28 15:01:28 +0200

    Settings page

--- resources/views/pages/auth/settings.blade.php
+++ resources/views/pages/auth/settings.blade.php
@@ -1,102 +1,89 @@
 @extends('layouts.app')
 
+@section('description')
+    <p class="mb-0">Your settings page allows you to change your password, or delete your account.</p>
+@endsection
+
 @section('content')
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">{{ __('Settings') }}</div>
+    <h1>Settings</h1>
 
-                <div class="card-body">
+    <x-card header="Change password" class="mb-4">
+        <form method="POST" action="{{ route('settings.password') }}">
+            @csrf
 
-                    <form method="POST" action="{{ route('settings.password') }}">
-                    @csrf
+            <div class="form-group row">
+                <label for="current_password" class="col-md-4 col-form-label text-md-right">{{ __('Current Password') }}</label>
 
-                    <div class="form-group row">
-                        <label for="current_password" class="col-md-4 col-form-label text-md-right">{{ __('Current Password') }}</label>
+                <div class="col-md-6">
+                    <input id="current_password" type="password" class="form-control @error('current_password') is-invalid @enderror" name="current_password" required>
 
-                        <div class="col-md-6">
-                            <input id="current_password" type="password" class="form-control @error('current_password') is-invalid @enderror" name="current_password" required>
-
-                            @error('current_password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('New Password') }}</label>
-
-                        <div class="col-md-6">
-                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                            @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm New Password') }}</label>
-
-                        <div class="col-md-6">
-                            <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                        </div>
-                    </div>
-
-                    <div class="form-group row mb-0">
-                        <div class="col-md-6 offset-md-4">
-                            <button type="submit" class="btn btn-primary">
-                                {{ __('Change Password') }}
-                            </button>
-                        </div>
-                    </div>
-                    </form>
-
+                    @error('current_password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
             </div>
-        </div>
-    </div>
 
-    <div class="row justify-content-center pt-3">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">{{ __('Delete Account') }}</div>
+            <div class="form-group row">
+                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('New Password') }}</label>
 
-                <div class="card-body">
+                <div class="col-md-6">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
-                    <form method="POST" action="{{ route('settings.delete-account') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                <span class="invalid-feedback" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('DELETE ACCOUNT (Permanent)') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
                 </div>
             </div>
-        </div>
-    </div>
 
+            <div class="form-group row">
+                <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm New Password') }}</label>
+
+                <div class="col-md-6">
+                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                </div>
+            </div>
+
+            <div class="form-group row mb-0">
+                <div class="col-md-6 offset-md-4">
+                    <button type="submit" class="btn btn-primary">
+                        {{ __('Change Password') }}
+                    </button>
+                </div>
+            </div>
+        </form>
+    </x-card>
+
+    <x-card header="Delete account" class="mb-4">
+        <form method="POST" action="{{ route('settings.delete-account') }}">
+            @csrf
+
+            <div class="form-group row">
+                <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                <div class="col-md-6">
+                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
+
+                    <small class="form-text text-muted">Deleting your account is permanent and removes all associated data from our database. We cannot restore accounts.</small>
+
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="form-group row mb-0">
+                <div class="col-md-6 offset-md-4">
+                    <button type="submit" class="btn btn-danger">
+                        Delete Account
+                    </button>
+                </div>
+            </div>
+        </form>
+    </x-card>
 @endsection
