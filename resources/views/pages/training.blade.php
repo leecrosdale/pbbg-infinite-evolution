ac120f80d63704912faa17028ee3a28f0463b91e
WaveHack <email@wavehack.net>
2021-08-28 14:21:30 +0200

    Training page

--- resources/views/pages/training.blade.php
+++ resources/views/pages/training.blade.php
@@ -1,34 +1,38 @@
 @extends('layouts.app')
 
+@section('description')
+    <p>The training page allows you to spend energy to train your attack and defence.</p>
+    <p class="mb-0">The amount gained each stat depends on energy spent, the training type, and a bit of RNG.</p>
+@endsection
+
 @section('content')
-    <x-card class="mb-0 mb-lg-4">
-        <x-slot name="header">
-            Training
-        </x-slot>
+    <h1>Training</h1>
 
-
-        <p>Train your attack and defence skill using energy.</p>
+    <x-card class="mb-4">
         <p>Light training will have a higher chance of success, but will yield worse results.</p>
         <p>Heavy training will yield better results but has a much higher chance of failure.</p>
-        <p></p>
 
         @if ($character->energy >= \App\Actions\TrainCharacterAction::MIN_ENERGY_TO_TRAIN)
+            <form method="post" action="{{ route('training.perform') }}">
+                @csrf
+                <span id="value">Using {{ $character->energy }} energy will take {{ $character->energy * \App\Calculator\TrainingCalculator::SECONDS_PER_ENERGY }} seconds to train</span>
 
+                <input type="range" min="{{ \App\Actions\TrainCharacterAction::MIN_ENERGY_TO_TRAIN }}" class="form-control" max="{{ $character->energy }}" value="{{ $character->energy }}" onchange="onChange(this.value)" name="energy">
 
-        <form method="post" action="{{ route('training.perform') }}">
-            @csrf
-            <span id="value">Using {{ $character->energy }} energy will take {{ $character->energy * \App\Calculator\TrainingCalculator::SECONDS_PER_ENERGY }} seconds to train</span>
-
-
-
-            <input type="range" min="{{ \App\Actions\TrainCharacterAction::MIN_ENERGY_TO_TRAIN }}" class="form-control" max="{{ $character->energy }}" value="{{ $character->energy }}" onchange="onChange(this.value)" name="energy">
-            <button type="submit" name="type" value="light" class="btn btn-primary">Light Training</button>
-            <button type="submit" name="type" value="average" class="btn btn-warning">Average Training</button>
-            <button type="submit" name="type" value="heavy" class="btn btn-danger">Heavy Training</button>
-        </form>
-
+                <div class="row">
+                    <div class="col-4">
+                        <button type="submit" name="type" value="light" class="btn btn-block btn-success">Light Training</button>
+                    </div>
+                    <div class="col-4">
+                        <button type="submit" name="type" value="average" class="btn btn-block btn-primary">Average Training</button>
+                    </div>
+                    <div class="col-4">
+                        <button type="submit" name="type" value="heavy" class="btn btn-block btn-danger">Heavy Training</button>
+                    </div>
+                </div>
+            </form>
         @else
-            You need more than {{ \App\Actions\TrainCharacterAction::MIN_ENERGY_TO_TRAIN }} energy to train!
+            <p>You need more than {{ \App\Actions\TrainCharacterAction::MIN_ENERGY_TO_TRAIN }} energy to train!</p>
         @endif
 
     </x-card>
@@ -36,7 +40,6 @@
 
 @push('scripts')
     <script>
-
         let secondsPerEnergy = {{ \App\Calculator\TrainingCalculator::SECONDS_PER_ENERGY }};
 
         function onChange(value) {
