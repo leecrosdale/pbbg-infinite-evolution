2569981e39b5e45f79317690a46821f4460edf34
WaveHack <email@wavehack.net>
2021-08-28 14:54:58 +0200

    Items page

--- resources/views/pages/items.blade.php
+++ resources/views/pages/items.blade.php
@@ -1,124 +1,176 @@
 @extends('layouts.app')
 
+@section('description')
+    <p>The items page shows you all items in your inventory, your equipped items, plus items you can craft at your current location.</p>
+@endsection
+
 @section('content')
-    <x-card header="Your Items" class="mb-4">
-        @if ($items->count() === 0)
-            <p class="mb-0">You have no items yet!</p>
-        @else
+    <h1>Inventory</h1>
+
+    @if ($items->count() === 0)
+        <p class="my-4 text-xl">You currently do not have any items.</p>
+    @else
+        <x-card class="mb-4">
             <x-slot name="bodyClass">p-0 table-responsive</x-slot>
 
-            <table class="table">
-                <thead>
-                <tr>
-                    <th>Evolution</th>
-                    <th>Name</th>
-                    <th>Type</th>
-                    <th>Qty</th>
-                    <th>Buffs</th>
-                    <th>Equipped</th>
-                    <th class="text-right">Action</th>
-                </tr>
+            <table class="table table-hover mb-0">
+                <thead class="text-white bg-secondary">
+                    <tr>
+                        <td>Item</td>
+                        <td>Quantity</td>
+                        <td>Buffs</td>
+                        <td width="130">Actions</td>
+                    </tr>
                 </thead>
                 <tbody>
-                @foreach ($items as $item)
-                    <tr>
-                        <td>{{ $item->evolution->name }}</td>
-                        <td>{{ snake_case_to_words($item->name) }}</td>
-                        <td>{{ snake_case_to_words($item->type) }}</td>
-                        <td>{{ $item->pivot->qty }}</td>
-                        <td>
-                            @if ($item->buffs)
-                                @foreach ($item->buffs as $buffName => $buffValue)
-                                        @if ($buffValue > 0)
-                                            <span class="badge badge-primary">{{ snake_case_to_words($buffName) }} +{{ number_format($buffValue) }}</span>
-                                        @elseif ($buffValue < 0)
-                                            <span class="badge badge-danger">{{ snake_case_to_words($buffName) }} {{ number_format($buffValue) }}</span>
-                                        @endif
-                                @endforeach
-                            @endif
-                        </td>
-                        <td>
-                            @if ($item->pivot->equipped)
-                                Equipped
-                            @endif
-                        </td>
-                        <td class="text-right align-middle">
-                        @if ($item->equippable)
-
-                                @if ($item->pivot->equipped)
-                                    <a href="{{ route('items.unequip', $item) }}"
-                                       class="btn btn-sm btn-primary">Un-Equip (-1e)</a>
-                                @else
-                                    <a href="{{ route('items.equip', $item) }}" class="btn btn-sm btn-primary">
-                                        Equip (-1e)
-                                    </a>
+                    @foreach ($items as $item)
+                        <tr>
+                            <td>
+                                <div class="d-flex align-items-center">
+                                    @if ($item->type === 'base')
+                                        <img src="{{ asset("img/icons/supplies/" . Str::slug($item->name) . ".svg") }}"
+                                             alt="{{ snake_case_to_words($item->type) }}"
+                                             class="mr-1"
+                                             style="height: 1rem; filter: drop-shadow(1px 1px 1px rgba(0, 0, 0, 0.25));">
+                                    @endif
+                                    <span class="font-weight-bold">{{ $item->name }}</span>
+                                    @if ($item->pivot->equipped)
+                                        <span class="badge badge-success ml-2">Equipped</span>
+                                    @endif
+                                </div>
+                                <small class="text-secondary">
+                                    {{ snake_case_to_words($item->type) }}
+                                    ({{ $item->evolution->name }})
+                                </small>
+                            </td>
+                            <td>
+                                {{ number_format($item->pivot->qty) }}
+                            </td>
+                            <td>
+                                @if ($item->buffs)
+                                    <div class="d-flex flex-wrap">
+                                        @php($statSprites = ['attack' => 'ra ra-sword', 'defence' => 'fas fa-shield-alt'])
+                                        @foreach ($item->buffs as $buffName => $buffValue)
+                                            <div class="d-flex align-items-center mr-2 text-{{ $buffName === 'attack' ? 'danger' : 'info' }}">
+                                                <i class="{{ $statSprites[$buffName] }}"></i>
+                                                @if ($buffValue > 0)
+                                                    +{{ number_format($buffValue) }}
+                                                @elseif ($buffValue < 0)
+                                                    {{ number_format($buffValue)  }}
+                                                @endif
+                                            </div>
+                                        @endforeach
+                                    </div>
                                 @endif
-                        @endif
-                        </td>
-                    </tr>
-                @endforeach
+                            </td>
+                            <td>
+                                @if ($item->equippable)
+                                    @if ($item->pivot->equipped)
+                                        <a href="{{ route('items.unequip', $item) }}" class="btn btn-block btn-danger">
+                                            <div>Un-Equip</div>
+                                            <small class="d-flex justify-content-center">
+                                                <div><i class="fas fa-bolt"></i> -1</div>
+                                            </small>
+                                        </a>
+                                    @else
+                                        <a href="{{ route('items.equip', $item) }}" class="btn btn-block btn-success">
+                                            <div>Equip</div>
+                                            <small class="d-flex justify-content-center">
+                                                <div><i class="fas fa-bolt"></i> -1</div>
+                                            </small>
+                                        </a>
+                                    @endif
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
-        @endif
-    </x-card>
+        </x-card>
+    @endif
 
-    <x-card header="Craft Items" class="mb-0 mb-lg-4">
-        @if ($craftableItems->count() === 0)
-            No craftable items available
-        @else
-            <x-slot name="bodyClass">p-1 table-responsive</x-slot>
+    <h2>Crafting</h2>
 
-            <table class="table">
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Type</th>
-                    <th>Recipe</th>
-                    <th>Buffs</th>
-                    <th class="text-right">Action</th>
-                </tr>
+    @if ($craftableItems->count() === 0)
+        <p class="my-4 text-xl">No craftable items available at this location.</p>
+    @else
+        <x-card class="mb-4">
+            <x-slot name="bodyClass">p-0 table-responsive</x-slot>
+
+            <table class="table table-hover mb-0">
+                <thead class="text-white bg-secondary">
+                    <tr>
+                        <td>Item</td>
+                        <td>Recipe</td>
+                        <td>Buffs</td>
+                        <td width="130">Actions</td>
+                    </tr>
                 </thead>
                 <tbody>
-                @foreach ($craftableItems as $item)
-                    <tr>
-                        <td>{{ $item->name }}</td>
-                        <td>{{ $item->type }}</td>
-                        <td>
-                            @if ($item->recipe)
-                                @foreach ($item->recipe as $recipe)
-                                    @if ($recipe->qty === 0)
-                                        @continue
-                                    @endif
+                    @foreach ($craftableItems as $item)
+                        <tr>
+                            <td>
+                                <div>
+                                    <span class="font-weight-bold">{{ $item->name }}</span>
+                                </div>
+                                <small class="text-secondary">
+                                    {{ snake_case_to_words($item->type) }}
+                                    ({{ $item->evolution->name }})
+                                </small>
+                            </td>
+                            <td>
+                                @if ($item->recipe)
+                                    <div class="d-flex flex-wrap">
+                                        @foreach ($item->recipe as $recipe)
+                                            @if ($recipe->qty === 0)
+                                                @continue
+                                            @endif
 
-                                    {{-- todo: we really need to use Eloquent relations for this :( N+1 issue atm --}}
-                                    @php($itemRecipe = \App\Models\Item::find($recipe->item_id))
+                                            {{-- todo: we really need to use Eloquent relations for this :( N+1 issue atm --}}
+                                            @php($itemRecipe = \App\Models\Item::find($recipe->item_id))
 
-                                    @if ($character->hasItemQty($itemRecipe, $recipe->qty))
-                                        <span class="badge badge-success">{{ $itemRecipe->name }}: {{ $recipe->qty }}</span>
-                                    @else
-                                        <span class="badge badge-danger">{{ $itemRecipe->name }}: {{ $recipe->qty }}</span>
-                                    @endif
-                                @endforeach
-                            @endif
-                        </td>
-                        <td>
-                            @if ($item->buffs)
-                                @foreach ($item->buffs as $k => $v)
-                                    @if ($v > 0)
-                                        <span class="badge badge-primary">{{ snake_case_to_words($k) }} +{{ $v }}</span>
-                                    @elseif ($v < 0)
-                                        <span class="badge badge-danger">{{ snake_case_to_words($k) }} {{ $v }}</span>
-                                    @endif
-                                @endforeach
-                            @endif
-                        </td>
-                        <td class="text-right align-middle">
-                            <a href="{{ route('items.craft', $item) }}" class="btn btn-sm btn-primary">Craft</a>
-                        </td>
-                    </tr>
-                @endforeach
+                                            <div class="d-flex align-items-center mr-2">
+                                                <img src="{{ asset("img/icons/supplies/" . Str::slug($itemRecipe->name) . ".svg") }}"
+                                                     alt="{{ snake_case_to_words($itemRecipe->type) }}"
+                                                     class="mr-1"
+                                                     style="height: 1rem; filter: drop-shadow(1px 1px 1px rgba(0, 0, 0, 0.25));">
+                                                <span class="text-{{ $character->hasItemQty($itemRecipe, $recipe->qty) ? 'success' : 'danger' }}">
+                                                {{ number_format($recipe->qty) }}
+                                            </span>
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                @endif
+                            </td>
+                            <td>
+                                @if ($item->buffs)
+                                    <div class="d-flex flex-wrap">
+                                        @php($statSprites = ['attack' => 'ra ra-sword', 'defence' => 'fas fa-shield-alt'])
+                                        @foreach ($item->buffs as $buffName => $buffValue)
+                                            <div class="d-flex align-items-center mr-2 text-{{ $buffName === 'attack' ? 'danger' : 'info' }}">
+                                                <i class="{{ $statSprites[$buffName] }}"></i>
+                                                @if ($buffValue > 0)
+                                                    +{{ number_format($buffValue) }}
+                                                @elseif ($buffValue < 0)
+                                                    {{ number_format($buffValue)  }}
+                                                @endif
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                @endif
+                            </td>
+                            <td>
+                                <a href="{{ route('items.craft', $item) }}" class="btn btn-block btn-primary">
+                                    <div>Craft</div>
+                                    <small class="d-flex justify-content-center">
+                                        <div><i class="fas fa-bolt"></i> -1</div>
+                                    </small>
+                                </a>
+                            </td>
+                        </tr>
+                    @endforeach
                 </tbody>
             </table>
-        @endif
-    </x-card>
+        </x-card>
+    @endif
 @endsection
