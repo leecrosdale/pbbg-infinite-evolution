f1e45816c6b6a2d291a11f82f37a9b1b916a0ea5
WaveHack <email@wavehack.net>
2021-08-29 16:35:34 +0200

    Add news section to dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -8,57 +8,79 @@
 @section('content')
     <h1>Dashboard</h1>
 
-    @if ($otherCharacters->count() > 0)
-        <x-card class="mb-4">
-            <x-slot name="bodyClass">p-0 table-responsive</x-slot>
+    <div class="row">
+        <div class="col-12 col-lg-6">
+            <h2>Players</h2>
 
-            <table class="table table-hover mb-0">
-                <thead class="text-white bg-secondary">
-                    <tr>
-                        <th>Player</th>
-                        <th class="text-right">Est. Strength</th>
-                        <th width="130">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($otherCharacters as $otherCharacter)
-                        <tr>
-                            <td>
-                                <div>
-                                    <span class="font-weight-bold">{{ $otherCharacter->name }}</span>
-                                </div>
-                                <small class="text-muted">Level {{ number_format($otherCharacter->level) }}</small>
-                            </td>
-                            <td class="text-right">
-                                @php($relativeStrength = ($otherCharacter->getPower() / $character->getPower()) * 100)
+            @if ($otherCharacters->count() > 0)
+                <x-card class="mb-4">
+                    <x-slot name="bodyClass">p-0 table-responsive</x-slot>
 
-                                @if ($relativeStrength < 75)
-                                    Weak
-                                @elseif ($relativeStrength > 125)
-                                    Strong
-                                @else
-                                    Equal
-                                @endif
-                            </td>
-                            <td>
-                                @php($energyCost = $attackCharacterCalculator->getEnergyCost($character))
-                                <form action="{{ route('character.attack', $otherCharacter) }}" method="POST">
-                                    @csrf
+                    <table class="table table-hover mb-0">
+                        <thead class="text-white bg-secondary">
+                            <tr>
+                                <th>Player</th>
+                                <th class="text-right">Est. Strength</th>
+                                <th width="130">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($otherCharacters as $otherCharacter)
+                                <tr>
+                                    <td>
+                                        <div>
+                                            <span class="font-weight-bold">{{ $otherCharacter->name }}</span>
+                                        </div>
+                                        <small class="text-muted">Level {{ number_format($otherCharacter->level) }}</small>
+                                    </td>
+                                    <td class="text-right">
+                                        @php($relativeStrength = ($otherCharacter->getPower() / $character->getPower()) * 100)
 
-                                    <button type="submit" class="btn btn-block btn-danger" {{ $character->energy < $attackCharacterCalculator->getEnergyCost($character) ? 'disabled' : null }}>
-                                        <div>Attack</div>
-                                        <small class="d-flex justify-content-center">
-                                            <div><i class="fas fa-bolt"></i> -{{ number_format($energyCost) }}</div>
-                                        </small>
-                                    </button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </x-card>
-    @else
-        <p class="my-4 text-xl">You don't see any other players here at {{ $location->name }}.</p>
-    @endif
+                                        @if ($relativeStrength < 75)
+                                            Weak
+                                        @elseif ($relativeStrength > 125)
+                                            Strong
+                                        @else
+                                            Equal
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @php($energyCost = $attackCharacterCalculator->getEnergyCost($character))
+                                        <form action="{{ route('character.attack', $otherCharacter) }}" method="POST">
+                                            @csrf
+
+                                            <button type="submit" class="btn btn-block btn-danger" {{ $character->energy < $attackCharacterCalculator->getEnergyCost($character) ? 'disabled' : null }}>
+                                                <div>Attack</div>
+                                                <small class="d-flex justify-content-center">
+                                                    <div><i class="fas fa-bolt"></i> -{{ number_format($energyCost) }}</div>
+                                                </small>
+                                            </button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </x-card>
+            @else
+                <p class="my-4 text-xl">You don't see any other players here at {{ $location->name }}.</p>
+            @endif
+        </div>
+        <div class="col-12 col-lg-6">
+            <h2>News</h2>
+
+            <x-card class="mb-4" header="Update: August 29th, 2021">
+                <p>Changes</p>
+                <ul>
+                    <li>Reduced experience gain from successful attacks.</li>
+                </ul>
+                <p>Fixes</p>
+                <ul class="mb-0">
+                    <li>Previous evolution-tier items not being craftable.</li>
+                </ul>
+            </x-card>
+        </div>
+    </div>
+
+
 @endsection
